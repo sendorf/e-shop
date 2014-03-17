6216ded874d589e364793bb8df97cf762a82a16d
Sergio Díaz <sendorf@gmail.com>
2014-03-17 23:07:08 +0100

    Finished the methods to pass the first test

--- Rspec/checkout_spec.rb
+++ Rspec/checkout_spec.rb
@@ -2,12 +2,12 @@
 require_relative '../checkout.rb'
 
 describe Checkout, "#Basket 1" do
-  it "The result should be £66.78." do
-    promotional_rules = {:rule1 => {:cond => "total >= 60", :result => "total = total - (total*10/100)"}}
+  it "The result should be 66.78." do
+    promotional_rules = {"rule1" => {:cond => "total >= 60", :result => "total = total - (total*10/100)"}}
     co = Checkout.new(promotional_rules)
-    co.scan(:001)
-    co.scan(:002)
-    co.scan(:003)
+    co.scan(001)
+    co.scan(002)
+    co.scan(003)
     co.total.should eq(66.78)
   end
 end
--- checkout.rb
+++ checkout.rb
@@ -1,4 +1,40 @@
 # checkout.rb
 class Checkout
 
+  attr_reader :promotional_rules, :articles, :items
+
+  def initialize(promotional_rules = {})
+    @promotional_rules = promotional_rules                                   # Initializes the promotional rules that may be applied
+    @articles = {1 => {:name => "Lavender heart", :price => 9.25},           # for the checkout
+                 2 => {:name => "Personalised cufflinks", :price => 45.00},  
+                 3 => {:name => "Kids T-shirt", :price => 19.95}}            # Initializes the articles in the shop
+    @items = []
+  end
+
+  def scan(item)
+    @items << items                                                         # Adds the key of every item to an array of keys
+  end                                                                       # of scanned items
+
+  def total()
+    total = 0
+    if(promotional_rules.size != 0)                                         # Checks if any of the rules is applicable before
+      promotional_rules.each do |key, rule|                                 # processing the total
+        if(eval(rule[:cond]))
+          eval(rule[:result])
+        end
+      end
+    end
+    @items.each do |item|                                                   # Calculates the total from the scanned items
+      article = @articles.values_at(item).first
+      total = article[:price] + total
+    end
+    if(promotional_rules.size != 0)                                         # Checks if any of the rules is applicable after
+      promotional_rules.each do |key, rule|                                 # processing the total
+        if(eval(rule[:cond]))
+          eval(rule[:result])
+        end
+      end
+    end
+    total
+  end
 end
