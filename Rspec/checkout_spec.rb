0c6aa8830a35787cd639d47e8b49a7e8ebbb6d45
Sergio Díaz <sendorf@gmail.com>
2014-03-17 20:08:46 +0100

    Created Rspec for the 1st basket

--- Rspec/checkout_spec.rb
+++ Rspec/checkout_spec.rb
@@ -1,3 +1,12 @@
 # checkout_spec.rb
 require_relative '../checkout.rb'
 
+describe Checkout, "#Basket 1" do
+  it "The result should be £66.78." do
+    co = Checkout.new(promotional_rules)
+    co.scan(:001)
+    co.scan(:002)
+    co.scan(:003)
+    co.total.should eq(66.78)
+  end
+end
