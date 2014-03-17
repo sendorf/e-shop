1e58407ab377192d4c4d83412fd2637bd839c4d2
Sergio Díaz <sendorf@gmail.com>
2014-03-17 20:23:41 +0100

    Added the first promotional rule

--- Rspec/checkout_spec.rb
+++ Rspec/checkout_spec.rb
@@ -3,6 +3,7 @@ require_relative '../checkout.rb'
 
 describe Checkout, "#Basket 1" do
   it "The result should be £66.78." do
+    promotional_rules = {:rule1 => {:cond => "total >= 60", :result => "total = total - (total*10/100)"}}
     co = Checkout.new(promotional_rules)
     co.scan(:001)
     co.scan(:002)
