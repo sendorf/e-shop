9b11421a0f2c36267dc07e51f7bda87269068a8e
Sergio Díaz <sendorf@gmail.com>
2014-03-27 08:24:50 +0100

    Created the rspec for the second basket

--- Rspec/checkout_spec.rb
+++ Rspec/checkout_spec.rb
@@ -11,3 +11,15 @@ describe Checkout, "#Basket 1" do
     co.total.should eq(66.78)
   end
 end
+
+describe Checkout, "#Basket 2" do
+  it "The result should be 36,95." do
+    promotional_rules = {"rule1" => {:cond => "total >= 60", :result => "total = total - (total*10/100)"},
+                         "rule2" => {:cond => '@items.count("001") >= 2', :result => '@articles["001"][:price] = 8.50'}}
+    co = Checkout.new(promotional_rules)
+    co.scan("001")
+    co.scan("003")
+    co.scan("001")
+    co.total.should eq(36,95)
+  end
+end
